a22f34a305a0a0f0466355578204503144938c7f
Chuck Dries <chuck@chuckdries.com>
2018-10-31 16:02:51 -0700

    socket.io hello world works

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,28 @@
 import express from 'express';
 import { Server } from 'http';
+import path from 'path';
+import socketio from 'socket.io';
 
 const app = express();
 const http = Server(app);
+const io = socketio(http);
 
 app.get('/', (req, res) => {
-  res.send('hello world!');
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-http.listen(3000, () => { console.log('listening on http://localhost:3000'); });
+io.on('connection', (socket) => {
+  console.log('a user connected'); // eslint-disable-line no-console
+  socket.on('disconnect', () => {
+    console.log('user disconnected'); // eslint-disable-line no-console
+  });
+  socket.on('chat message', (msg) => {
+    console.log(`message: ${msg}`); // eslint-disable-line no-console
+    io.emit('chat message', msg);
+  });
+});
+
+
+http.listen(3000, () => {
+  console.log('listening on http://localhost:3000'); // eslint-disable-line no-console
+});
